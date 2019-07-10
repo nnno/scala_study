d45d5e0fc5c37ed9292280ed702f16d2fd5d64e8
nnno <nnno@nnno.jp>
2019-07-10 17:40:56 +0900

    05. n-gram, 06. 集合 の回答を追加

--- src/test/scala/scala/study/nlp100/Chapter01Spec.scala
+++ src/test/scala/scala/study/nlp100/Chapter01Spec.scala
@@ -57,5 +57,50 @@ class Chapter01Spec extends FunSpec with Matchers {
     }.toMap
     assert(result == expect)
   }
+
+  // 単語単位のn-gram関数
+  def ngram(target:  List[String], n: Int): List[List[String]] = target.sliding(n).toList
+  // 文字単位のn-gram関数
+  def ngram(target: => List[Char], n: Int): List[String]       = target.sliding(n).map(p => p.mkString).toList
+
+  it("05. n-gram") {
+    val target = "I am an NLPer"
+    val chars = target.toList
+    val words = target.split(" ").toList
+
+    // 文字単位のuni-gram, bi-gram, tri-gram
+    assert(ngram(chars, 1) == List("I", " ", "a", "m", " ", "a", "n", " ", "N", "L", "P", "e", "r"))
+    assert(ngram(chars, 2) == List("I ", " a", "am", "m ", " a", "an", "n ", " N", "NL", "LP", "Pe", "er"))
+    assert(ngram(chars, 3) == List("I a", " am", "am ", "m a", " an", "an ", "n N", " NL", "NLP", "LPe", "Per"))
+
+    // 単語単位のuni-gram, bi-gram, tri-gram
+    assert(ngram(words, 1) == List(List("I"), List("am"), List("an"), List("NLPer")))
+    assert(ngram(words, 2) == List(List("I", "am"), List("am", "an"), List("an", "NLPer")))
+    assert(ngram(words, 3) == List(List("I", "am", "an"), List("am", "an", "NLPer")))
+  }
+
+  it("06. 集合") {
+    val x = ngram("paraparaparadise".toList, 2).toSet
+    val y = ngram("paragraph".toList, 2).toSet
+    // x, y
+    assert(x               == Set("ar", "se", "di", "is", "pa", "ap", "ad", "ra"))
+    assert(y               == Set("ar", "gr", "ph", "ra", "pa", "ap", "ag"))
+    // 和集合
+    assert((x union y)     == Set("ar", "gr", "se", "ph", "di", "is", "pa", "ap", "ad", "ra", "ag"))
+    // 積集合
+    assert((x intersect y) == Set("ar", "ap", "pa", "ra"))
+    // 差集合
+    assert((x diff y)      == Set("di", "is", "se", "ad"))
+    // xに"se"が含まれるか (含まれる)
+    assert(x contains "se")
+    // yに"se"が含まれるか (含まれない)
+    assert(!(y contains "se"))
+  }
+
+  it("07. テンプレートによる文生成") {
+    def func(x: Int, y: String, z: Double) = s"${x}時の${y}は$z"
+    assert(func(12, "気温", 22.4) == "12時の気温は22.4")
+  }
+
 }
 
